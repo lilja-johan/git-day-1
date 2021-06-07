085df993e6fbdae887baaaa77732b42d4fd7a28d
lilja-johan <jplilja93@gmail.com>
2021-06-07 11:44:53 +0200

    Increment method updated

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int number;
-        number = incrementOne(1);
+        number = incrementOne(0);
         System.out.println(number);
     }
 
     public static int incrementOne(int i) {
-        return i++;
+        return i + 1;
     }
 }
